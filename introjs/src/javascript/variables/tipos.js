f13c0c7ac43a95aaa3ab879371269f37b141d735
martin <codemcm@hotmail.com>
2021-01-11 13:57:09 -0600

    tipos

---
+++ introjs/src/javascript/variables/tipos.js
@@ -0,0 +1,11 @@
+/**
+ * Javascript tiene seis tipos primitivos:
+ *  Sin definir (undefined),
+ * Nulo (null),
+ * Lógicos (boolean),
+ * Numérico (number),
+ * Cadena (string),
+ * Símbolo (symbol).
+ * Todos los demás tipos son objetos
+ * (Object): Array, Date, Promise, etc.
+ */
